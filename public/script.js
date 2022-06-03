c55f661a099f971c0d82236a2a1b9c11eb55bcf6
siosw <github@simonoswald.xyz>
2022-06-03 14:47:22 +0200

    refactors rendering of comments and replies to use the same function

--- public/script.js
+++ public/script.js
@@ -32,36 +32,60 @@ function getRandomAuthor() {
   return authors[Math.floor(Math.random() * authors.length)]
 }
 
-function renderReplies(comment, commentId) {
+function renderComment(comment, commentId, replyId) {
+  const commentDiv = document.createElement('div')
+  if (comment.isReply) commentDiv.classList.add('w-[55vw]')
+  else commentDiv.classList.add('w-[60vw]')
+  commentDiv.classList.add('px-8', 'py-2', 'border', 'rounded', 'relative')
+  commentDiv.id = commentId
+
+  const commentText = document.createElement('p')
+  commentText.innerText = comment.text
+  
+  const commentAuthor = document.createElement('h4')
+  commentAuthor.classList.add('font-bold')
+  commentAuthor.innerText = comment.author
+
+  const commentInteractions = document.createElement('div')
+  const root = ReactDOM.createRoot(commentInteractions)
+  root.render(
+    <div>
+      { !comment.isReply ? (
+      <div className="flex space-x-2 items-center" >
+      <Upvote 
+        id={ commentId }
+        initialUpvoteCount={ comment.upvotes }
+        socket={ socket }
+      />
+      <Reply 
+        id={ commentId }
+      />
+      </div>
+      ) : (
+      <div className="flex space-x-2 items-center" >
+      <ReplyUpvote 
+        commentId={ commentId }
+        replyId={ replyId }
+        initialUpvoteCount={ comment.upvotes }
+        socket={ socket }
+      />
+      </div>
+      )}
+    </div>
+
+  )
+  commentDiv.appendChild(commentAuthor)
+  commentDiv.appendChild(commentText)
+  commentDiv.appendChild(commentInteractions)
+
+  return commentDiv
+}
+
+function loadReplies(comment, commentId) {
   const commentsDiv = document.getElementById('comments')
 
   comment.replies.forEach((r, i) => {
-    const replyDiv = document.createElement('div')
-    replyDiv.classList.add('w-[55vw]', 'px-8', 'py-2', 'ml-10', 'border', 'rounded', 'relative')
-
-    const replyText = document.createElement('p')
-    replyText.innerText = r.text
-    
-    const replyAuthor = document.createElement('h4')
-    replyAuthor.classList.add('font-bold')
-    replyAuthor.innerText = r.author
-
-    const replyInteractions = document.createElement('div')
-    const root = ReactDOM.createRoot(replyInteractions)
-    root.render(
-      <div className="flex space-x-2 items-center">
-        <ReplyUpvote 
-          commentId={ commentId }
-          replyId={ i }
-          initialUpvoteCount={ r.upvotes }
-          socket={ socket }
-        />
-      </div>
-    )
-
-    replyDiv.appendChild(replyAuthor)
-    replyDiv.appendChild(replyText)
-    replyDiv.appendChild(replyInteractions)
+    const replyDiv = renderComment(r, commentId, i)
     commentsDiv.appendChild(replyDiv)
   })
 }
@@ -73,39 +97,9 @@ async function loadComments() {
   commentsDiv.innerHTML = ''
 
   comments.forEach((c, i) => {
-    const commentDiv = document.createElement('div')
-    commentDiv.classList.add('w-[60vw]', 'px-8', 'py-2', 'border', 'rounded', 'relative')
-    commentDiv.id = i
-
-    const commentText = document.createElement('p')
-    commentText.innerText = c.text
-    
-    const commentAuthor = document.createElement('h4')
-    commentAuthor.classList.add('font-bold')
-    commentAuthor.innerText = c.author
-
-    const commentInteractions = document.createElement('div')
-    const root = ReactDOM.createRoot(commentInteractions)
-    root.render(
-      <div className="flex space-x-2 items-center">
-        <Upvote 
-          id={ i }
-          initialUpvoteCount={ c.upvotes }
-          socket={ socket }
-        />
-        <Reply 
-          id={ i }
-        />
-      </div>
-    )
-
-    commentDiv.appendChild(commentAuthor)
-    commentDiv.appendChild(commentText)
-    commentDiv.appendChild(commentInteractions)
-  
+    const commentDiv = renderComment(c, i)
     commentsDiv.appendChild(commentDiv)
-
-    renderReplies(c, i)
+    loadReplies(c, i)
   })
 }
 
